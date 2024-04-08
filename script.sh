cf83b4e3cf898e34ed5907c35bda615cf35144fe
wildan <wildanakhmad3584@gmail.com>
2024-04-08 18:40:58 +0700

    initial commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+# FROM Instruction
+
+docker build -t akhmadwildan/from from
+
+docker image ls
+
+# RUN Instruction
+docker build -t akhmadwildan/run run
+
+docker build -t akhmadwildan/run run --progress=plain --no-cache
