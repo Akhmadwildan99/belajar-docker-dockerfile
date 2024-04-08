4849eab4a5451f1188a5e893e429ec9d8162cfec
wildan <wildanakhmad3584@gmail.com>
2024-04-08 19:03:32 +0700

    command instruction

--- script.sh
+++ script.sh
@@ -8,3 +8,14 @@ docker image ls
 docker build -t akhmadwildan/run run
 
 docker build -t akhmadwildan/run run --progress=plain --no-cache
+
+# CMD Instruction
+docker build -t akhmadwildan/command command
+
+docker image inspect akhmadwildan/command
+
+docker container create --name command akhmadwildan/command
+
+docker container start command
+
+docker container logs command
